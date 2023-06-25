68369c54c54623e5076b3528bd2f17cf41635272
tarunkatariaonline <tarunsoftsol@gmail.com>
2023-06-25 17:29:34 +0530

    logout functionalityf

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const dotenv = require('dotenv')
 dotenv.config({path:'./config.env'})
 app.use(cors({
   
-  origin:"http://localhost:3000",
+  origin:"https://ecommerceindia.vercel.app",
   credentials:true,
 }))
 
